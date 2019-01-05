42a43ccd84134c78a8884efe5e417e9c76e882f5
Jean-Baptiste Mazon <jb@yavin>
2019-01-06 00:08:55 +0100

    Day 14 cleanup

---
+++ day14/README.txt
@@ -0,0 +1,7 @@
+Chocolate Charts
+
+I'm reading both the puzzle statement and my code and I can think of
+nothing to say or comment about any of them.  I suppose this is just
+another read-and-implement case.
+
+So nothing to see here, move along.
--- day14/day14.hs
+++ day14/day14.hs
@@ -8,22 +8,24 @@ s0 = ((0,1),Q.fromList [3,7])
 step ((p1,p2),s) = ((p1',p2'),s') where
   v1 = Q.index s p1
   v2 = Q.index s p2
-  q = Q.fromList (map digitToInt (show (v1+v2)))
+  q = Q.fromList $ map digitToInt $ show $ v1 + v2
   s' = s <> q
   p1' = (p1 + 1 + v1) `mod` Q.length s'
   p2' = (p2 + 1 + v2) `mod` Q.length s'
 
-part1 n = take 10 $ drop n full
+part1 = map intToDigit . take 10 . flip drop full . read
 
 full = go (map snd $ iterate step s0) 0 where
   go qs@(q:qs') i | i < Q.length q = Q.index q i : go qs (i+1)
-                  | otherwise = go qs' i
+                  | otherwise      =               go qs' i
 
 part2 ns = go full 0 where
-  go xs i | ns `isPrefixOf` xs = i
-          | otherwise = go (tail xs) $! i+1
+  go xs i | (digitToInt <$> ns) `isPrefixOf` xs = i
+          | otherwise                           = go (tail xs) $! i+1
 
 main = do
-  [n] <- getArgs
-  putStrLn $ map intToDigit $ part1 (read n)
-  print $ part2 (map digitToInt n)
+  args <- getArgs
+  n <- case args of [n] -> return n
+                    []  -> filter isDigit <$> getContents
+  putStrLn $ part1 n
+  print $ part2 n
