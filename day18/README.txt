8b8d10b9469ac3288550c41bfa708e5587d4a081
Jean-Baptiste Mazon <jb@yavin>
2019-01-06 23:38:50 +0100

    Day 18 cleanup

---
+++ day18/README.txt
@@ -0,0 +1,5 @@
+Settlers of the North Pole
+
+Part one was a simple implementation.  Part two was an easier version
+of the loop detection of day 12: no normalization or offsetting
+necessary, who cares if keying on 2D data.
--- day18/day18.hs
+++ day18/day18.hs
@@ -3,17 +3,15 @@ import Data.Array
 import qualified Data.Map as M
 import Control.Arrow
 
-import Debug.Trace
-
 main = do
   ss <- iterate step . parse <$> getContents
   print $ uncurry (*) . count . elems $ ss !! 10
   print $ uncurry (*) . count . elems $ unloop ss 1000000000
 
-parse input = listArray ((1,1),(h,w)) (concat ls) where
-  ls = lines input
-  h = length ls
-  w = length (head ls)
+parse input = listArray ((1,1),(h,w)) (concat ls)
+  where ls = lines input
+        h = length ls
+        w = length (head ls)
 
 step life = listArray (bounds life) $ map new (indices life) where
   new p = case s of '.' -> if ts >= 3 then '|' else '.'
@@ -30,6 +28,6 @@ showR m = unlines [ [ m!(i,j) | j <- [jMin..jMax] ] | i <- [iMin..iMax] ]
 count = (length *** length) . partition (== '|') . filter (/= '.')
 
 unloop xs0 n = go M.empty xs0 0 where
-  go cl xs@(x:xs') i | i == n = trace "Direct" x
-                     | Just j <- M.lookup x cl = traceShow (j,i) $ xs !! ((n-i) `mod` (i-j))
+  go cl xs@(x:xs') i | i == n = x
+                     | Just j <- M.lookup x cl = xs !! ((n-i) `mod` (i-j))
                      | otherwise = go (M.insert x i cl) xs' $! i+1
