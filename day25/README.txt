040e3968d9792cbfa6835b1dfaf7c6083c922c8e
Jean-Baptiste Mazon <jb@yavin>
2019-01-07 12:10:39 +0100

    Day 25 cleanup

---
+++ day25/README.txt
@@ -0,0 +1,5 @@
+Four-Dimensional Adventure
+
+Less than 1500 units, a simple pairwise connect in a disjoint set
+structure was all that was needed.  I didn't even put in path
+compression.
--- day25/day25.hs
+++ day25/day25.hs
@@ -3,23 +3,20 @@ import qualified Data.Map as M
 import Text.Regex.Posix
 import Control.Monad
 
-import Debug.Trace
-
 type DS = (M.Map Vec Vec,M.Map Vec Int)
-data Vec = V !Int !Int !Int !Int deriving (Show,Eq,Ord)
+data Vec = V !Int !Int !Int !Int deriving (Eq,Ord)
 
 main :: IO ()
-main = interact $ show . part1 . map parse . lines
+main = interact $ show . solve . map parse . lines
 
 parse :: String -> Vec
 parse l = V x y z w where
   [x,y,z,w] = map read $ getAllTextMatches $ l =~ "-?[0-9]+"
 
-part1 :: [Vec] -> Int
-part1 vs = length $ nub $ map (fst . rep ds') vs
-  where
+solve :: [Vec] -> Int
+solve vs = length $ nub $ map (fst . rep ds') vs where
     ds0 = (M.fromList (map (join (,)) vs),M.fromList (map (flip (,) 1) vs))
-    ds' = foldl' connect ds0 $ filter (uncurry connected) $ pairs vs :: DS
+    ds' = foldl' connect ds0 $ filter (uncurry connected) $ pairs vs
     pairs ss = [ (s1,s2) | (s1:ss') <- tails ss, s2 <- ss' ]
     connected = ((<= 3) .) . dist
 
@@ -27,21 +24,18 @@ dist :: Vec -> Vec -> Int
 dist (V a b c d) (V e f g h) = abs (e-a) + abs (f-b) + abs (g-c) + abs (h-d)
 
 connect :: DS -> (Vec,Vec) -> DS
-connect ds@(rp,hg) (e1,e2) -- | traceShow (e1,e2) False = undefined
-                           | r1 == r2 = ds
+connect ds@(rp,hg) (e1,e2) | r1 == r2  = ds
                            | otherwise = (M.insert rb ra rp,
                                           if h1 == h2
                                           then M.adjust succ ra hg
                                           else hg)
-  where
-    (r1,h1) = rep ds e1
-    (r2,h2) = rep ds e2
-    (ra,rb) | h1 < h2 = (r2,r1)
-            | otherwise = (r1,r2)
+  where (r1,h1) = rep ds e1
+        (r2,h2) = rep ds e2
+        (ra,rb) | h1 < h2 = (r2,r1)
+                | otherwise = (r1,r2)
     
 rep :: DS -> Vec -> (Vec,Int)
-rep ds@(rp,hg) e -- | traceShow e False = undefined
-                 | r == e = (r,h)
+rep ds@(rp,hg) e | r == e    = (r,h)
                  | otherwise = rep ds r
   where Just r = M.lookup e rp
         Just h = M.lookup e hg
